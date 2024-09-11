36beec8a4e595599b0babf34af1c7bd63fc54e60
TOFAYEL H TOPU <tofayelhtopu@gmail.com>
2024-09-11 22:03:18 +0600

    basic layout directory structure

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Header from "@/layouts/components/AppHeader.vue";
+import Sidebar from "@/layouts/components/AppSidebar.vue";
+import Footer from "@/layouts/components/AppFooter.vue";
+</script>
+
+<template>
+  <div>
+    <!--  BEGIN NAVBAR  -->
+    <Header></Header>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN MAIN CONTAINER  -->
+    <div class="main-container" id="container">
+      <!--  BEGIN SIDEBAR  -->
+      <Sidebar></Sidebar>
+      <!--  END SIDEBAR  -->
+
+      <!--  BEGIN CONTENT AREA  -->
+      <div id="content" class="main-content">
+        <router-view />
+
+        <!-- BEGIN FOOTER -->
+        <Footer></Footer>
+        <!-- END FOOTER -->
+      </div>
+      <!--  END CONTENT AREA  -->
+    </div>
+  </div>
+</template>
---
+++ src/layouts/components/AppFooter.vue
@@ -0,0 +1,24 @@
+<script setup>
+ // code
+</script>
+
+<template>
+  <!-- BEGIN FOOTER -->
+  <div class="footer-wrapper">
+    <div class="footer-section f-section-1">
+      <p class="">Copyright © 2024 <a target="_blank" href="https://github.com/tofayel5">Tofayel H Topu</a>, All rights reserved.</p>
+    </div>
+  </div>
+  <!-- END FOOTER -->
+</template>
+
+<style lang="scss" scoped>
+.footer-wrapper .footer-section {
+  p {
+    color: #888ea8;
+    a {
+      color: #888ea8;
+    }
+  }
+}
+</style>
